1ffece67057d9e662be72ac5057058a486529a41
Clara Mille Kalø <claramillekaloe@Claras-MacBook-Pro-2.local>
2020-10-17 15:58:33 +0200

    fourth commit

--- hardUser.js
+++
@@ -1,9 +0,0 @@
-const user = require('./user')
-
-let userArray = []
-let user1 = new user ('Peter', 'Jakobsen', 27, 'male', ['soccer', 'cooking', 'reading']);
-let user2 = new user ('Nicole', 'Sørensen', 31, 'woman', ['knitting', 'baking', 'running', 'yoga']);
-
-userArray = [user1,user2];
-
-module.exports = userArray
--- server.js
+++ server.js
@@ -1,1 +1,26 @@
+// denne angiver porten, og hvilke routes/ endpoints der findes dertil
+const express= require ("express");
+const server=express();
+const port= process.env.port || 3000;
+
+
+// mine GET requests: 
+const users = require("./controller/");
+server.use("./model/user", users)
+
+const interest= require("./controller/interestController.js");
+app.use("/interests", interest)
+
+const match= require("./controller/matchController.js");
+app.use("/matches", match)
+
+
+//server aktiveres
+app.listen(port, err =>{
+    if (err)
+{
+    return console.log("ERROR", err);}
+    console.log("listening on port ${port}");
+});
+
 
